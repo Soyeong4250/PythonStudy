64e5508cee2fc1922a105520742d9426ee928e70
이소영 <th42500@naver.com>
2023-04-19 23:58:04 +0900

    [Feat] 2023.04.20 - 함수 학습 진행중

--- 07/03-function.py
+++ 07/03-function.py
@@ -7,10 +7,10 @@ def test1(msg = "안녕하세요"):
 def test2(*a):  # positional argument : 인자가 몇 개일지 모를 때 사용
     print(f"{a}")
 
-def test3(**a):  # positional argument : 인자가 몇 개일지 모를 때 사용
-  print(f"{a}")
+def test3(**a):  # keyword argument : 인자에 값을 전달할 대, 이름을 붙여서 전달하는 방식 (dictionary 자료형과 유사)
+    print(f"{a}")
 
 test("안녕하세요")
 test1()
 test2("aaa", "bbb", "ccc")
-test3("aaa", "bbb", "ccc")
+test3(name = "aaa")
